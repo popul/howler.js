1f7987ac68ea98cdc5f314feacb71e17459a58a7
popul <cpopul@gmail.com>
2015-01-19 23:24:07 +0100

    Update howler.core.js

--- src/howler.core.js
+++ src/howler.core.js
@@ -433,7 +433,7 @@
       }
 
       // Determine how long to play for and where to start playing.
-      var seek = sound._seek > 0 ? sound._seek : self._sprite[sprite][0] / 1000;
+      var seek = sound._seek > 0 ? sound._seek + self._sprite[sprite][0] / 1000 : self._sprite[sprite][0] / 1000;
       var duration = ((self._sprite[sprite][0] + self._sprite[sprite][1]) / 1000) - seek;
 
       // Should this sound loop?
